3c94d5c0ca399fffaab88c59811ca8c89f8248e5
Masum-Reza <careerhunter4280@gmail.com>
2023-08-28 18:21:57 -0700

    added modal to show particular phone detail

--- js/engin.js
+++ js/engin.js
@@ -14,11 +14,11 @@ let phoneLoader = async (isShowAll) => {
     handleLoader(true)
 
     let searchField = document.getElementById('searchField');
-    let searchText = searchField.value;
-    if (!searchText) {
-        alert('Enter phone name');
-        return;
-    }
+    let searchText = searchField.value || 'oppo';
+    // if (!searchText) {
+    //     alert('Enter phone name');
+    //     return;
+    // }
 
     let phones = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     let phone = await phones.json();
@@ -43,13 +43,14 @@ let showPhone = (phone,isShowAll) => {
         showAllBtn.classList.add('hidden')
     }
 
-    console.log('is show all', isShowAll)
+    // console.log('is show all', isShowAll)
     //showing particuler number of phone
     if(!isShowAll){
         phoneList = phoneList.slice(0, 12)
     }
 
     phoneList.forEach(element => {
+        // console.log(element)
 
         let div = document.createElement('div');
         div.classList = 'card bg-base-100 shadow-xl'
@@ -59,9 +60,9 @@ let showPhone = (phone,isShowAll) => {
             </figure>
             <div class="card-body items-center text-center">
                 <h2 class="card-title">${element.phone_name}</h2>
-                <p>${element.slug}</p>
+                <p>${element.brand}</p>
                 <div class="card-actions">
-                    <button class="btn btn-success">Buy Now</button>
+                    <button onclick="handleShowDetail('${element.slug}')" class="btn btn-success text-white">Show Details</button>
                 </div>
             </div>
         `
@@ -77,3 +78,26 @@ let showPhone = (phone,isShowAll) => {
 let handleShowAll = () => {
     phoneLoader(true);
 }
+
+//handleShowDetail function
+let handleShowDetail = async (id) =>{
+    console.log('clicked', id)
+    let res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    let data = await res.json();
+    let particulerData = data.data;
+    console.log(particulerData)
+
+    showIndividualModal(particulerData)
+}
+
+let showIndividualModal = (particulerData) =>{
+
+    let barndName = document.getElementById('barndName');
+    barndName.innerText = `${particulerData.name}`
+    
+    showDetails_modal.showModal();
+}
+
+
+// showing default phones 
+phoneLoader()
