6f657e031d7907b41cca156ecb68c9a20d2f9e94
Masum-Reza <careerhunter4280@gmail.com>
2023-08-28 19:28:58 -0700

    added error handler

--- js/engin.js
+++ js/engin.js
@@ -23,12 +23,12 @@ let phoneLoader = async (isShowAll) => {
     let phones = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     let phone = await phones.json();
 
-    showPhone(phone , isShowAll)
-    
+    showPhone(phone, isShowAll)
+
 }
 
 
-let showPhone = (phone,isShowAll) => {
+let showPhone = (phone, isShowAll) => {
     let phoneList = phone.data;
 
     let phoneContainer = document.getElementById('phoneContainer');
@@ -46,32 +46,36 @@ let showPhone = (phone,isShowAll) => {
 
     // console.log('is show all', isShowAll)
     //showing particuler number of phone
-    if(!isShowAll){
+    if (!isShowAll) {
         phoneList = phoneList.slice(0, 12)
     }
 
-    phoneList.forEach(element => {
-        // console.log(element)
+    if (phoneList.length > 0) {
+        phoneList.forEach(element => {
+            // console.log(element)
 
-        let div = document.createElement('div');
-        div.classList = 'card bg-base-100 shadow-xl'
-        div.innerHTML = `
-        <figure class="px-10 pt-10">
-                <img src="${element.image}" alt="Shoes" class="rounded-xl" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">${element.phone_name}</h2>
-                <p>${element.brand}</p>
-                <div class="card-actions">
-                    <button onclick="handleShowDetail('${element.slug}')" class="btn btn-success text-white">Show Details</button>
+            let div = document.createElement('div');
+            div.classList = 'card bg-base-100 shadow-xl'
+            div.innerHTML = `
+            <figure class="px-10 pt-10">
+                    <img src="${element.image}" alt="Shoes" class="rounded-xl" />
+                </figure>
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">${element.phone_name}</h2>
+                    <p>${element.brand}</p>
+                    <div class="card-actions">
+                        <button onclick="handleShowDetail('${element.slug}')" class="btn btn-success text-white">Show Details</button>
+                    </div>
                 </div>
-            </div>
-        `
-        phoneContainer.appendChild(div)
-    });
+            `
+            phoneContainer.appendChild(div)
+        });
 
-    //loading ends here
-    handleLoader(false)
+        //loading ends here
+        handleLoader(false)
+    } else {
+        alert('No Data available!')
+    }
 
 }
 
@@ -81,7 +85,7 @@ let handleShowAll = () => {
 }
 
 //handleShowDetail function
-let handleShowDetail = async (id) =>{
+let handleShowDetail = async (id) => {
     // console.log('clicked', id)
     let res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
     let data = await res.json();
@@ -93,7 +97,7 @@ let handleShowDetail = async (id) =>{
 
 
 //individual phone show modal element passing
-let showIndividualModal = (particulerData) =>{
+let showIndividualModal = (particulerData) => {
 
     let showDetailsContainer = document.getElementById('showDetailsContainer');
     showDetailsContainer.innerHTML = `
